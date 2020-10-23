a829a612bea0602fd1997b3e26f0d57d1de3ce3a
adriandevid <adriandevid36@gmail.com>
2020-10-22 22:03:57 -0300

    Tela

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 function IniciarPercusoDeInstrucoes() {
-	console.log(document.getElementById("Objeto").getAttribute("cx") = 1000);
+   	intervalosDeChamandaDeFuncao = setInterval(game, VelocidadeDeChamadaDeFuncao);
+}
+function game()
+{
+	ContadorDeEspacoPercorrido += 100;
+	ObjectMov.setAttribute("cx", ContadorDeEspacoPercorrido);
+	if(ContadorDeEspacoPercorrido == 1000) {
+   		clearInterval(intervalosDeChamandaDeFuncao);
+   		console.log("fim de jogo!!");
+   		ObjectMov.setAttribute("cx", 1200);
+   		ContadorDeEspacoPercorrido = 0;
+   	}
 }
 function AdicionarComandoNaMemoria() {
 
