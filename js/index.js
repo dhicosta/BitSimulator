0edff26ef957727c435c22787f6d19bdead98d83
M4Y50N <pethersonreis@gmail.com>
2020-10-24 01:09:15 -0300

    Formatação

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 function IniciarPercusoDeInstrucoes() {
 	intervalosDeChamandaDeFuncao = setInterval(game, VelocidadeDeChamadaDeFuncao);
 }
+
 function game() {
 	ContadorDeEspacoPercorrido += 100;
 	ObjectMov.setAttribute("cx", ContadorDeEspacoPercorrido);
+
 	if (ContadorDeEspacoPercorrido == 1000) {
 		clearInterval(intervalosDeChamandaDeFuncao);
 		console.log("fim de jogo!!");
@@ -11,5 +13,7 @@ function game() {
 		ContadorDeEspacoPercorrido = 0;
 	}
 }
+
 function AdicionarComandoNaMemoria() {}
+
 function ExecutarPercursoEmAleatorio() {}
